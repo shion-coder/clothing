6088220b1f02cdba02970c9aa9d091fd77b1e18f
Shion <shionchill@Shions-MacBook-Pro.local>
2020-05-15 15:11:37 +0700

    Updates

---
+++ precache-manifest.4519974824f86720f0a46debeb95c5bd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57eee1cf3dda442bd5aaf4bc5f858366",
+    "url": "/clothing/index.html"
+  },
+  {
+    "revision": "f1e2a1a8382b32a01cc9",
+    "url": "/clothing/static/js/2.0bcdc516.chunk.js"
+  },
+  {
+    "revision": "dcd1d80362aef046a5450470cca1218a",
+    "url": "/clothing/static/js/2.0bcdc516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023a40a27063dabacc4f",
+    "url": "/clothing/static/js/3.678a16da.chunk.js"
+  },
+  {
+    "revision": "258678bfb4ebce909dea",
+    "url": "/clothing/static/js/4.49056606.chunk.js"
+  },
+  {
+    "revision": "d7187d68eb947ff329ef",
+    "url": "/clothing/static/js/5.328f294a.chunk.js"
+  },
+  {
+    "revision": "3a2e20dbfd70a9aea0bf",
+    "url": "/clothing/static/js/6.87dc6ca2.chunk.js"
+  },
+  {
+    "revision": "4701ae7fba5eb34d64e2",
+    "url": "/clothing/static/js/7.81b52848.chunk.js"
+  },
+  {
+    "revision": "b171d1df49c2edcb0155",
+    "url": "/clothing/static/js/8.000ff48b.chunk.js"
+  },
+  {
+    "revision": "6d7d7dcfdaae8431a11c",
+    "url": "/clothing/static/js/9.4928f3a1.chunk.js"
+  },
+  {
+    "revision": "43aa8d6cfb516317e35c",
+    "url": "/clothing/static/js/main.8fa5dd25.chunk.js"
+  },
+  {
+    "revision": "fe2f360eec98d207aafc",
+    "url": "/clothing/static/js/runtime-main.e9d834d8.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing/static/media/clothing.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
